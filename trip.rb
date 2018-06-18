5f88391f7bcc5ce15c00d92851762eefd39a6cf1
Reaganhatt <reagan.hatt@gmail.com>
2018-06-18 20:04:37 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -2,13 +2,17 @@ puts "Let's plan your trip! Where would you like to stay?"
 stay = "Orlando
   "
 stay = gets.chomp.capitalize
+puts "I would like to stay in #{stay}."
+puts "Where would you like to eat?"
 eat = "Chick-fil-a
 "
+puts "I would like to eat at #{eat}."
 eat = gets.chomp.capitalize
+puts "How long will you be staying?"
 nights = "8 nights
 "
 nights = gets.chomp
-puts "I would like to stay in #{stay} for #{nights}. I would like to eat at #{eat}."
+puts "I will be staying for #{nights}."
 
 
 
